959b148721883003e8376709d122fc797e84cc96
RickMcConney <rick.mcconney@gmail.com>
2023-08-01 10:46:59 -0500

    add wifi auto connect

--- connect5.sh
+++ connect5.sh
@@ -7,6 +7,7 @@ for INTERFACE in $ifaces;
          sudo nmcli d disconnect $INTERFACE
          sudo nmcli con delete wifi5
          sudo nmcli d wifi connect $1 password $2 ifname $INTERFACE name wifi5
+         sudo nmcli device set $INTERFACE autoconnect yes
          nmcli -f IP4.ADDRESS dev show $INTERFACE
       fi
   done;
--- connect6.sh
+++ connect6.sh
@@ -7,6 +7,7 @@ for INTERFACE in $ifaces;
          sudo nmcli d disconnect $INTERFACE
          sudo nmcli con delete wifi6
          sudo nmcli d wifi connect $1 password $2 ifname $INTERFACE name wifi6
+         sudo nmcli device set $INTERFACE autoconnect yes
          nmcli -f IP4.ADDRESS dev show $INTERFACE
       fi
   done;
